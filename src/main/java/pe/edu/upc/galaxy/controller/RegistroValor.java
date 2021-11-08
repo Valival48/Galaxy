718b2b00cc6194c4010c6f24ccf457c65cc9cad8
Valeria <v.castillorevelo@gmail.com>
2021-11-07 20:43:46 -0500

    correcion y linkeo de html

--- src/main/java/pe/edu/upc/galaxy/controller/RegistroValor.java
+++ src/main/java/pe/edu/upc/galaxy/controller/RegistroValor.java
@@ -1,6 +1,44 @@
 package pe.edu.upc.galaxy.controller;
 
-public class RegistroValor {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+import pe.edu.upc.galaxy.business.ValorService;
+import pe.edu.upc.galaxy.entity.Valor;
+
+@Controller
+@RequestMapping("/registros")
+@SessionAttributes("valor")
+public class RegistroValor {
+	@Autowired
+	private ValorService valorService;
+	
+	@GetMapping
+	public String list(Model model) {
+		try {
+			List<Valor> valores = valorService.getAll();
+			model.addAttribute("valores", valores);
+		} catch (Exception e) {
+	
+			e.printStackTrace();
+		}		
+		return "registros/Registro_Valores";		
+	}
+	
+	public String agregar (Model model) {
+		try {
+		model.addAttribute("valor", new Valor());
+		
+		}catch(	Exception e) {}
+		
+		return "registros/Registro_Valores";
+	}
+	
 	
 }

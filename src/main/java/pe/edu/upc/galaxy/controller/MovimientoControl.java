15246ee0b2504577300f1250169f9246c38193e3
oshioku19 <oshioku@outlook.com.pe>
2021-11-19 05:24:42 -0500

    Listas
    lista de cartera y descuento

---
+++ src/main/java/pe/edu/upc/galaxy/controller/MovimientoControl.java
@@ -0,0 +1,33 @@
+package pe.edu.upc.galaxy.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import pe.edu.upc.galaxy.business.ClienteService;
+import pe.edu.upc.galaxy.entity.Valor;
+
+@Controller
+@RequestMapping("/movimiento")
+public class MovimientoControl {
+
+	@Autowired
+	private ClienteService clienteService;
+	
+	public String newValor(Model model, @ModelAttribute("valor") Valor valor) {
+		try {
+			
+			model.addAttribute("clientes",clienteService.listClientes());
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.err.println(e.getMessage());
+		}
+
+		return "Movimiento_comitentes";
+	}
+}
--- src/main/java/pe/edu/upc/galaxy/controller/PrincipalController.java
+++ src/main/java/pe/edu/upc/galaxy/controller/PrincipalController.java
@@ -1,21 +1,31 @@
 package pe.edu.upc.galaxy.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import pe.edu.upc.galaxy.business.ClienteService;
+import pe.edu.upc.galaxy.business.TipoMonedaService;
+import pe.edu.upc.galaxy.business.ValorService;
+import pe.edu.upc.galaxy.entity.TipoMoneda;
+import pe.edu.upc.galaxy.entity.Valor;
 
 @Controller
 @RequestMapping("/")
 public class PrincipalController {
+	
+
 
 	@GetMapping
 	public String home() {
 		return "menu";
 	}
-	@GetMapping("inicio")
-	public String home2() {
-		return "Movimiento_comitentes";
-	}
+	
 	@GetMapping("new")
 	public String home4() {
 		return "menu";
@@ -24,5 +34,7 @@ public class PrincipalController {
 	public String monto() {
 		return "MontoExtranjero";
 	}
+	
+	
 	
 }

f1f272a14e27f6d18256a1bef42affb3a4d8c8b5
oshioku19 <oshioku@outlook.com.pe>
2021-11-24 00:58:46 -0500

    Redondeo de decimal

--- src/main/java/pe/edu/upc/galaxy/controller/RegistroValor.java
+++ src/main/java/pe/edu/upc/galaxy/controller/RegistroValor.java
@@ -1,5 +1,7 @@
 package pe.edu.upc.galaxy.controller;
 
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
 import java.util.List;
 
 import javax.validation.Valid;
@@ -14,7 +16,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
-
 import pe.edu.upc.galaxy.business.ClienteService;
 import pe.edu.upc.galaxy.business.TipoMonedaService;
 import pe.edu.upc.galaxy.business.ValorService;
@@ -28,27 +29,25 @@ import pe.edu.upc.galaxy.entity.Valor;
 public class RegistroValor {
 	@Autowired
 	private ValorService valorService;
-	
+
 	@Autowired
 	private TipoMonedaService tipoMonedaService;
-	
+
 	@Autowired
 	private ClienteService clienteService;
-	
-	
 
 	@GetMapping("newValor")
 	public String newValor(Model model) {
 		try {
-			List<TipoMoneda> monedas= tipoMonedaService.getAll();
+			List<TipoMoneda> monedas = tipoMonedaService.getAll();
 			List<Valor> valores = valorService.getAll();
-		/*	model.addAttribute("monedas",tipoMonedaService.listAllMoneda());*/
-			model.addAttribute("clientes",clienteService.listClientes());
+			/* model.addAttribute("monedas",tipoMonedaService.listAllMoneda()); */
+			model.addAttribute("clientes", clienteService.listClientes());
 			model.addAttribute("valores", valores);
 			model.addAttribute("monedas", monedas);
-			
-			model.addAttribute("valor",new Valor());
-			
+
+			model.addAttribute("valor", new Valor());
+
 		} catch (Exception e) {
 			e.printStackTrace();
 			System.err.println(e.getMessage());
@@ -58,14 +57,19 @@ public class RegistroValor {
 	}
 
 	@PostMapping("saveValor")
-	public String saveValor(Model model, @Valid @ModelAttribute("valor") Valor valor
-			,BindingResult result) {
+	public String saveValor(Model model, @Valid @ModelAttribute("valor") Valor valor, BindingResult result) {
+
 		if (result.hasErrors()) {
 		}
 
 		try {
-			Valor valorSaved = new Valor ();
-			valor.setDia((int)((valor.getFechaVencimiento().getTime()-valor.getFechaEmision().getTime())/(1000 * 3600 * 24)));
+		double val= ((valor.getFechaVencimiento().getTime() - valor.getFechaEmision().getTime())
+				/ (1000 * 3600 * 24));
+			DecimalFormatSymbols separadoresPersonalizados = new DecimalFormatSymbols();
+			separadoresPersonalizados.setDecimalSeparator('.');
+			DecimalFormat formato2 = new DecimalFormat("#.##", separadoresPersonalizados);
+			Valor valorSaved = new Valor();
+		 valor.setDia(Integer.parseInt(formato2.format(val)));
 			valorService.create(valor);
 			model.addAttribute("valor", valorSaved);
 		} catch (Exception e) {
